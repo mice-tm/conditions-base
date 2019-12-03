8657a7619abcd32f7a94c39886f6ba760d45352b
mice <imychkova@gmail.com>
2019-12-03 12:43:40 +0200

    small fix

--- src/models/constructor/attributes/TimestampAttribute.php
+++ src/models/constructor/attributes/TimestampAttribute.php
@@ -6,9 +6,7 @@ use micetm\conditionsBase\models\AttributeInterface;
 class TimestampAttribute extends AbstractAttribute implements AttributeInterface
 {
     /** @var array */
-    public $comparisons = self::availableComparisons;
-
-    const availableComparisons =  [
+    public $comparisons = [
         self::EQUAL_TO_COMPARISON => self::EQUAL_TO_COMPARISON,
         self::GREATER_THAN_COMPARISON => self::GREATER_THAN_COMPARISON,
         self::GREATER_THAN_OR_EQUAL_TO_COMPARISON => self::GREATER_THAN_OR_EQUAL_TO_COMPARISON,
